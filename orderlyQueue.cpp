fd6bb6db2cca14d546775cad9054693a57746955
dishajk <djkuzhively@gmail.com>
2022-11-06 19:35:11 +0530

    testing push

---
+++ orderlyQueue.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+string orderlyQueue(string s, int k);
+
+int main ()
+{
+	return 0;
+}
+
+string orderlyQueue(string s, int k){
+	return 0;
+}
