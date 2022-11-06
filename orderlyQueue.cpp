0bf7234b729f2fc274b76242e5341151bd1bfb96
dishajk <djkuzhively@gmail.com>
2022-11-06 23:18:54 +0530

    half done

--- orderlyQueue.cpp
+++ orderlyQueue.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <cstdlib>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -8,9 +8,32 @@ string orderlyQueue(string s, int k);
 
 int main ()
 {
+	cout<<orderlyQueue("cba",1);
+
 	return 0;
 }
 
 string orderlyQueue(string s, int k){
-	return 0;
+
+	vector<char> lexi(s.begin(), s.end());	//store string s in vector lexi
+	string previous;
+
+	do{
+	string previous(lexi.begin(),lexi.end());
+	//Choose the biggest char among the first k characters
+	int big = 0;
+	for(int i = 1; i < k; i++){
+		if(lexi[i] > lexi[big]){
+			big = i;
+		}
+	}
+	//Append the chosen character to end
+	lexi.emplace_back(lexi[big]);
+	lexi.erase(lexi.begin()+big);
+	string current(lexi.begin(),lexi.end());
+	cout << current;
+	}
+	while(previous < current); //until current < previous
+	//return previous 
+	return previous;
 }
