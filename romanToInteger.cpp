13a3fa04f3f33a8ff2efa37f7839014181cd5e83
dishajk <djkuzhively@gmail.com>
2022-11-06 21:27:08 +0530

    problem #13 into unordered map in c++

--- romanToInteger.cpp
+++ romanToInteger.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <cstdlib>
-#include <string>
+#include <unordered_map>
 
 using namespace std;
 
 int romantoint(string s);
-int arabic (char roman);
 
 int main ()
 {
@@ -18,34 +16,24 @@ int main ()
 
 int romantoint(string s) {
 
-	int number = arabic(s[0]);
+	unordered_map<char,int> arabic;
+
+	arabic['I'] = 1;
+	arabic['V'] = 5;
+	arabic['X'] = 10;
+	arabic['L'] = 50;
+	arabic['C'] = 100;
+	arabic['D'] = 500;
+	arabic['M'] = 1000;
+
+	int number = arabic[s[0]];
 	for (int i = 1; i < s.length(); i++){
-		if (arabic(s[i]) > arabic(s[i-1])){	
-			number -= arabic(s[i-1]);
-			number += arabic(s[i])-arabic(s[i-1]);
+		if (arabic[s[i]] > arabic[s[i-1]]){	
+			number -= arabic[s[i-1]];
+			number += arabic[s[i]]-arabic[s[i-1]];
 		}else{
-		number += arabic(s[i]);
+		number += arabic[s[i]];
 		}
 	}
 	return number;
-}
-
-int arabic (char roman){
-	if (roman == 'I'){
-		return 1;
-	}else if (roman == 'V'){
-		return 5;
-	}else if (roman == 'X'){
-		return 10;
-	}else if (roman == 'L'){
-		return 50;
-	}else if (roman == 'C'){
-		return 100;
-	}else if (roman == 'D'){
-		return 500;
-	}else if (roman == 'M'){
-		return 1000;
-	}else{
-		return 0;
-	}
 }
