9f5248d45fc6b4dc0ec00fd7423a52bbf83fbfd5
dishajk <djkuzhively@gmail.com>
2022-11-07 22:58:22 +0530

    wrong solution #899

--- orderlyQueue.cpp
+++ orderlyQueue.cpp
@@ -1,39 +1,44 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 string orderlyQueue(string s, int k);
+string vectorCharToString(vector<char> charVec);
 
 int main ()
 {
-	cout<<orderlyQueue("cba",1);
-
+	string result = orderlyQueue("kuh",1);
+	cout << "result is\t" << result << "\n";
 	return 0;
 }
 
 string orderlyQueue(string s, int k){
 
 	vector<char> lexi(s.begin(), s.end());	//store string s in vector lexi
-	string previous;
 
-	do{
-	string previous(lexi.begin(),lexi.end());
-	//Choose the biggest char among the first k characters
-	int big = 0;
-	for(int i = 1; i < k; i++){
-		if(lexi[i] > lexi[big]){
-			big = i;
-		}
+// Find the smallest k chars in s
+// push letter in the back until any small enters first k
+// 	check if smallest, if yes push until another small enters, if second smallest continue pushing, if not push back the small until second smallest comes and so on.
+//
+}
+
+string vectorCharToString(vector<char> charVec){
+	string result;
+		transform(charVec.begin(), charVec.end(), back_inserter(result),
+				[](char c)
+			{
+				return c;
+				});
+		return result;
+
+}
+
+int lexiCheck(vector<char> word, int m){
+	int n = 0;
+	for (int i = 1; i < m, i++){
+		if (word[i] < word[i-1])
 	}
-	//Append the chosen character to end
-	lexi.emplace_back(lexi[big]);
-	lexi.erase(lexi.begin()+big);
-	string current(lexi.begin(),lexi.end());
-	cout << current;
-	}
-	while(previous < current); //until current < previous
-	//return previous 
-	return previous;
 }
